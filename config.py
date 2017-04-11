e04bd17f1c2f872654672cfce64d8d7dd8d4efc9
gao14g <gao14g@email.unc.edu>
2017-04-10 21:59:06 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ config_header = 'Slack-Eve-Killboard/1.0a https://github.com/gao14g/Slack_Eve_Ki
 config_owner = [{'allianceID':99006343, 'allianceID':99006918,'allianceID':99004344,'corporationID':98040755,'corporationID':98290394,'allianceID':1354830081}] # Format of: 'corporationID': <id> or 'allianceID': <id> (can be multiple)
 config_check = '86400' # Check for the last day
 config_sleep_time = 120 # Delay between checks
-config_slack_url = 'https://hooks.slack.com/services/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' # Slack integration code
+config_slack_url = 'https://hooks.slack.com/services/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' # Slack integration code
 config_run_as_daemon = False
 
 ### Locales
