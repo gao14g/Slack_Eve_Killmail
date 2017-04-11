048ca062198438b76f8b1cc39d95e9d40709469d
gao14g <gao14g@email.unc.edu>
2017-04-10 21:39:54 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 config_header = 'Slack-Eve-Killboard/1.0a https://github.com/dimurgos/Slack-Eve-Killmail' # header
-config_owner = [{'': 0}] # Format of: 'corporationID': <id> or 'allianceID': <id> (can be multiple)
+config_owner = [{'allianceID':99006343}, {'allianceID':99006918},{'allianceID':99004344},{'corporationID':98040755},{'corporationID':98290394},{'allianceID':1354830081}] # Format of: 'corporationID': <id> or 'allianceID': <id> (can be multiple)
 config_check = '86400' # Check for the last day
 config_sleep_time = 1200 # Delay between checks
 config_slack_url = 'https://hooks.slack.com/services/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' # Slack integration code
